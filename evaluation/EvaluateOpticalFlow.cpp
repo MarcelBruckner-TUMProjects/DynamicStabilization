eb54e2c75a869340df0bf967acb39c10dedde825
brucknem <brucknem>
2022-04-09 14:29:21 +0200

    Final commit

--- evaluation/EvaluateOpticalFlow.cpp
+++ evaluation/EvaluateOpticalFlow.cpp
@@ -20,130 +20,150 @@ using namespace dynamic_stabilization::evaluation;
  */
 class Setup : public ::VideoSetup {
 private:
-	/**
-	 * The matcher used to match the features.
-	 */
-	std::vector<dynamic_stabilization::stabilization::DynamicStabilizationBase> stabilizers;
-	std::vector<std::string> stabilizerNames = {
-		"SURF",
+    /**
+     * The matcher used to match the features.
+     */
+    std::vector<dynamic_stabilization::stabilization::DynamicStabilizationBase> stabilizers;
+    std::vector<std::string> stabilizerNames = {
+            "SURF",
 //		"ORB",
 //		"FAST"
-	};
-	std::vector<dynamic_stabilization::opticalflow::FarnebackDenseOpticalFlow> opticalFlows;
+    };
 
-	std::shared_ptr<::CSVWriter> csvWriter;
-	std::shared_ptr<::CSVWriter> timeWriter;
-	int frameId = -1;
-	bool withMask = false;
-	int padding = 10;
-	boost::filesystem::path evaluationPath;
-	bool writeBadFrames = false;
-	int warmup = 30;
+    std::vector<std::string> opticalFlowNames = {
+            "Original"
+    };
+    std::vector<dynamic_stabilization::opticalflow::FarnebackDenseOpticalFlow> opticalFlows;
+
+    std::shared_ptr<::CSVWriter> csvWriter;
+    std::shared_ptr<::CSVWriter> timeWriter;
+    int frameId = -1;
+    bool withMask = false;
+    int padding = 10;
+    boost::filesystem::path evaluationPath;
+    bool writeBadFrames = false;
+    int warmup = 30;
+
+    int fontSize = 3;
 
 public:
-	explicit Setup() : VideoSetup() {
-		stabilizers.emplace_back(dynamic_stabilization::stabilization::SURFBFDynamicStabilization{});
+    explicit Setup() : VideoSetup() {
+        stabilizers.emplace_back(dynamic_stabilization::stabilization::SURFBFDynamicStabilization{});
 //		stabilizers.emplace_back(dynamic_stabilization::stabilization::ORBBFDynamicStabilization{});
 //		stabilizers.emplace_back(dynamic_stabilization::stabilization::FastFREAKBFDynamicStabilization{});
 
-		for (int i = 0; i <= stabilizers.size(); i++) {
-			opticalFlows.emplace_back();
-		}
-	}
+        for (const auto &stabilizerName : stabilizerNames) {
+            opticalFlowNames.emplace_back("Stabilized [" + stabilizerName + "]");
+        }
 
-	void init() override {
-		VideoSetup::init();
+        for (int i = 0; i <= stabilizers.size(); i++) {
+            opticalFlows.emplace_back();
+        }
+    }
 
-		evaluationPath =
-			outputFolder / "DynamicStabilization" / boost::filesystem::path(inputResource).filename().string();
-		if (!boost::filesystem::is_directory(evaluationPath)) {
-			boost::filesystem::create_directories(evaluationPath);
-		}
+    void init() override {
+        VideoSetup::init();
 
-		csvWriter = std::make_shared<::CSVWriter>(evaluationPath / (boost::filesystem::path(inputResource).filename()
-																		.string() + getNowSuffix() + ".csv"));
+        evaluationPath =
+                outputFolder / "DynamicStabilization" / boost::filesystem::path(inputResource).filename().string();
+        if (!boost::filesystem::is_directory(evaluationPath)) {
+            boost::filesystem::create_directories(evaluationPath);
+        }
 
-		timeWriter = std::make_shared<::CSVWriter>(evaluationPath / (boost::filesystem::path(inputResource).filename()
-																		 .string() + "_times_" + getNowSuffix() +
-																	 ".csv"));
-		*csvWriter << "Frame" << "Original";
-		for (const auto &name : stabilizerNames) {
-			*csvWriter << name;
-		}
-		*csvWriter << newline;
-		*timeWriter << "Frame" << "Original";
-		for (const auto &name : stabilizerNames) {
-			*timeWriter << name;
-		}
-		*timeWriter << newline;
-	}
+        csvWriter = std::make_shared<::CSVWriter>(evaluationPath / (boost::filesystem::path(inputResource).filename()
+                                                                            .string() + getNowSuffix() + ".csv"));
 
-	void calculateOpticalFlows() {
-		std::vector<cv::cuda::GpuMat> frames{pad(frameGPU, padding)};
-		auto size = pad(frameGPU, padding).size();
+        timeWriter = std::make_shared<::CSVWriter>(evaluationPath / (boost::filesystem::path(inputResource).filename()
+                                                                             .string() + "_times_" + getNowSuffix() +
+                                                                     ".csv"));
+        *csvWriter << "Frame" << "Original";
+        for (const auto &name : stabilizerNames) {
+            *csvWriter << name;
+        }
+        *csvWriter << newline;
+        *timeWriter << "Frame" << "Original";
+        for (const auto &name : stabilizerNames) {
+            *timeWriter << name;
+        }
+        *timeWriter << newline;
 
-		std::vector<cv::cuda::GpuMat> backgroundMasks{stabilizers[0].getBackgroundMask(size)};
+        cv::createTrackbar("Font Size", windowName, &fontSize, 5);
+    }
 
-		for (const auto &stabilizer :stabilizers) {
-			frames.emplace_back(pad(stabilizer.getStabilizedFrame(), padding));
-			backgroundMasks.emplace_back(pad(stabilizer.getBackgroundMask(size), padding));
-		}
+    void calculateOpticalFlows() {
+        std::vector<cv::cuda::GpuMat> frames{pad(frameGPU, padding)};
+        auto size = pad(frameGPU, padding).size();
 
-		int i = 0;
-		for (auto &opticalFlow : opticalFlows) {
-			if (withMask) {
-				opticalFlow.calculate(frames[i], backgroundMasks[i]);
-			} else {
-				opticalFlow.calculate(frames[i]);
-			}
-			i++;
-		}
-	}
+        std::vector<cv::cuda::GpuMat> backgroundMasks{stabilizers[0].getBackgroundMask(size)};
 
-	void specificMainLoop() override {
-		frameId++;
+        for (const auto &stabilizer :stabilizers) {
+            frames.emplace_back(pad(stabilizer.getStabilizedFrame(), padding));
+            backgroundMasks.emplace_back(stabilizer.getBackgroundMask(size));
+        }
+
+        int i = 0;
+        for (auto &opticalFlow : opticalFlows) {
+            if (withMask) {
+                opticalFlow.calculate(frames[i], backgroundMasks[i]);
+            } else {
+                opticalFlow.calculate(frames[i]);
+            }
+            i++;
+        }
+    }
+
+    void specificMainLoop() override {
+        frameId++;
 
 //		std::vector<cv::Mat> frames{
-//			::pad(::addText(frameCPU, "Frame: " + std::to_string(frameId), 2, 5, 5), padding)
+//			::pad(::addText(frameCPU, "Frame: " + std::to_string(frameId), fontSize, 5, 5), padding)
 //		};
 
-		std::vector<cv::Mat> frames{
-			::pad(frameCPU, padding)
-		};
+        std::vector<cv::Mat> frames{
+                ::addText(::pad(frameCPU, padding), "Original", fontSize, 5, 5)
+        };
 
-		*timeWriter << frameId << 0;
+        *timeWriter << frameId << 0;
 
-		int i = 0;
-		for (auto &stabilizer :stabilizers) {
-			stabilizer.stabilize(frameGPU);
-			*timeWriter << stabilizer.getTotalMilliseconds();
-//
-//			frames.emplace_back(::pad(
-//				::addRuntimeToFrame(cv::Mat(stabilizer.getStabilizedFrame()), stabilizerNames[i], stabilizer
-//					.getTotalMilliseconds(), 2, 5, 5), padding)
-//			);
+        int i = 0;
+        for (auto &stabilizer :stabilizers) {
+            stabilizer.stabilize(frameGPU);
+            *timeWriter << stabilizer.getTotalMilliseconds();
 
-			frames.emplace_back(::pad(cv::Mat(stabilizer.getStabilizedFrame()), padding));
-			i++;
-		}
-		*timeWriter << newline;
+            frames.emplace_back(::pad(
+                    ::addRuntimeToFrame(cv::Mat(stabilizer.getStabilizedFrame()),
+                                        "Stabilized [" + stabilizerNames[i] + "]", stabilizer
+                                                .getTotalMilliseconds(), fontSize, 5, 5), padding)
+            );
 
-		calculateOpticalFlows();
+//			frames.emplace_back(::pad(cv::Mat(stabilizer.getStabilizedFrame()), padding));
+            i++;
+        }
+        *timeWriter << newline;
+
+        calculateOpticalFlows();
+        cv::hconcat(frames, finalFrame);
+
+        frames.clear();
+        i = 0;
+        for (auto &opticalFlow : opticalFlows) {
+            auto opticalFlowDrawnFrame = opticalFlow.draw();
+            opticalFlowDrawnFrame = ::addText(opticalFlowDrawnFrame, "Optical Flow", fontSize, 5, 5);
+            std::stringstream shift;
+            shift << std::fixed << std::setprecision(3) << opticalFlow.getMagnitudeMean();
+            opticalFlowDrawnFrame = ::addText(opticalFlowDrawnFrame,
+                                              opticalFlowNames[i] + " - Mean pixel shift: " +
+                                              shift.str() + " px",
+                                              fontSize, 5, 5 + fontSize * 24);
+            frames.emplace_back(opticalFlowDrawnFrame);
+
+            i++;
+        }
+
 //		cv::hconcat(frames, finalFrame);
+        cv::hconcat(frames, bufferCPU);
+        finalFrame = ::vconcat({finalFrame, bufferCPU});
 
-		frames.clear();
-		for (auto &opticalFlow : opticalFlows) {
-//			frames.emplace_back(
-//				::addText(opticalFlow.draw(),
-//						  "Mean pixel shift: " + std::to_string(opticalFlow.getMagnitudeMean()), 2, 5, 5)
-//			);
-			frames.emplace_back(opticalFlow.draw());
-		}
-
-		cv::hconcat(frames, finalFrame);
-//		cv::hconcat(frames, bufferCPU);
-//		finalFrame = ::vconcat({finalFrame, bufferCPU});
-//
 //		if (withMask) {
 //			frames.clear();
 //			frames.emplace_back(::cvtColor(stabilizers[0].getBackgroundMask(frameGPU.size()), cv::COLOR_GRAY2BGR));
@@ -192,14 +212,14 @@ public:
 //		if (write && writeBadFrames) {
 //			cv::imwrite((evaluationPath / (frameName + ".png")).string(), finalFrame);
 //		}
-		*csvWriter << newline;
-	}
+        *csvWriter << newline;
+    }
 };
 
 int main(int argc, char const *argv[]) {
-	Setup setup;
-	setup.fromCLI(argc, argv);
-	setup.setRenderingScaleFactor(0.4);
-	setup.mainLoop();
-	return 0;
+    Setup setup;
+    setup.fromCLI(argc, argv);
+    setup.setRenderingScaleFactor(0.4);
+    setup.mainLoop();
+    return 0;
 }
--- evaluation/VisualizeFeatureMatching.cpp
+++ evaluation/VisualizeFeatureMatching.cpp
@@ -14,26 +14,27 @@
  */
 class Setup : public dynamic_stabilization::evaluation::VideoSetup {
 private:
-	/**
-	 * The current frame feature detector applied in the main loop.
-	 */
-	std::shared_ptr<dynamic_stabilization::stabilization::detection::FeatureDetectionBase> frameDetector, referenceFrameDetector;
+    /**
+     * The current frame feature detector applied in the main loop.
+     */
+    std::shared_ptr<dynamic_stabilization::stabilization::detection::FeatureDetectionBase> frameDetector, referenceFrameDetector;
 
-	/**
-	 * The matcher used to match the features.
-	 */
-	std::shared_ptr<dynamic_stabilization::stabilization::matching::FeatureMatchingBase> matcher, matcherWithoutFundamental;
+    /**
+     * The matcher used to match the features.
+     */
+    std::shared_ptr<dynamic_stabilization::stabilization::matching::FeatureMatchingBase> matcher, matcherWithoutFundamental;
 
 public:
-	explicit Setup() : VideoSetup() {
+    explicit Setup() : VideoSetup() {
 //        dynamic_stabilization::features::SIFTFeatureDetection detector(100);
 //        frameDetector = std::make_shared<dynamic_stabilization::features::SIFTFeatureDetection>(detector);
 //        referenceFrameDetector = std::make_shared<dynamic_stabilization::features::SIFTFeatureDetection>(detector);
 
-		dynamic_stabilization::stabilization::detection::SURFFeatureDetection detector;
-		frameDetector = std::make_shared<dynamic_stabilization::stabilization::detection::SURFFeatureDetection>(detector);
-		referenceFrameDetector = std::make_shared<dynamic_stabilization::stabilization::detection::SURFFeatureDetection>(
-			detector);
+        dynamic_stabilization::stabilization::detection::SURFFeatureDetection detector;
+        frameDetector = std::make_shared<dynamic_stabilization::stabilization::detection::SURFFeatureDetection>(
+                detector);
+        referenceFrameDetector = std::make_shared<dynamic_stabilization::stabilization::detection::SURFFeatureDetection>(
+                detector);
 
 //        dynamic_stabilization::features::ORBFeatureDetection detector(1000);
 //        frameDetector = std::make_shared<dynamic_stabilization::features::ORBFeatureDetection>(detector);
@@ -43,42 +44,44 @@ public:
 //        frameDetector = std::make_shared<dynamic_stabilization::features::FastFREAKFeatureDetection>(detector);
 //        referenceFrameDetector = std::make_shared<dynamic_stabilization::features::FastFREAKFeatureDetection>(detector);
 
-		matcher = std::make_shared<dynamic_stabilization::stabilization::matching::BruteForceFeatureMatching>(cv::NORM_L2);
-		matcherWithoutFundamental = std::make_shared<dynamic_stabilization::stabilization::matching::BruteForceFeatureMatching>
-			(cv::NORM_L2);
-		matcherWithoutFundamental->setShouldUseFundamentalMatrix(false);
+        matcher = std::make_shared<dynamic_stabilization::stabilization::matching::BruteForceFeatureMatching>(
+                cv::NORM_L2);
+        matcherWithoutFundamental = std::make_shared<dynamic_stabilization::stabilization::matching::BruteForceFeatureMatching>
+                (cv::NORM_L2);
+        matcherWithoutFundamental->setShouldUseFundamentalMatrix(false);
 //        matcher = std::make_shared<dynamic_stabilization::features::BruteForceFeatureMatching>(cv::NORM_HAMMING);
 //        matcher = std::make_shared<dynamic_stabilization::features::FlannFeatureMatching>(true);
 
+    }
 
-	}
+    void specificMainLoop() override {
+        writeFrames = true;
+        outputFolder = "visualize_feature_matching";
+        frameDetector->detect(frameGPU);
+        if (referenceFrameDetector->isEmpty()) {
+            referenceFrameDetector->detect(frameGPU);
+        }
 
-	void specificMainLoop() override {
-		frameDetector->detect(frameGPU);
-		if (referenceFrameDetector->isEmpty()) {
-			referenceFrameDetector->detect(frameGPU);
-		}
-
-		matcher->match(frameDetector, referenceFrameDetector);
+        matcher->match(frameDetector, referenceFrameDetector);
 //		matcherWithoutFundamental->match(frameDetector, referenceFrameDetector);
 //		totalAlgorithmsDuration = frameDetector->getTotalMilliseconds() + matcher->getTotalMilliseconds() +
 //								  matcherWithoutFundamental->getTotalMilliseconds();
 //
 //		cv::vconcat(matcherWithoutFundamental->draw(), matcher->draw(), finalFrame);
 
-		finalFrame = matcher->draw();
-	}
+        finalFrame = matcher->draw();
+    }
 
-	void specificAddMessages() override {
+    void specificAddMessages() override {
 //		addRuntimeToFinalFrame("Feature detection", frameDetector->getTotalMilliseconds(), 5, 20);
 //		addRuntimeToFinalFrame("Feature matching", matcher->getTotalMilliseconds(), 5, 40);
-	}
+    }
 };
 
 int main(int argc, char const *argv[]) {
-	Setup setup;
-	setup.fromCLI(argc, argv);
-	setup.setRenderingScaleFactor(1);
-	setup.mainLoop();
-	return 0;
+    Setup setup;
+    setup.fromCLI(argc, argv);
+    setup.setRenderingScaleFactor(1);
+    setup.mainLoop();
+    return 0;
 }
